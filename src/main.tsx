c866a5d731690a2a1229721cdb250399e2f873ec
Sergio Luis Alfonso Pinzon <salfonso@fgs.co>
2025-04-20 21:56:34 -0500

    fix: reorganize base directories react app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Core from "@/core";
 
-createRoot(document.getElementById('root')!).render(
+import "@styles/main.scss";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Core />
+  </StrictMode>
+);
